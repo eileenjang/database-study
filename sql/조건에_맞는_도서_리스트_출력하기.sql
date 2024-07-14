796ae39ffc856ea06f6ecc4d587528bd90741c27
eileen jang <eileen.jang2139@gmail.com>
2024-07-14 21:30:32 +0900

    add sql

--- sql/조건에_맞는_도서_리스트_출력하기.sql
+++ sql/조건에_맞는_도서_리스트_출력하기.sql
@@ -1,1 +1,3 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/144853
+
+SELECT BOOK_ID, DATE_FORMAT(PUBLISHED_DATE, "%Y-%m-%d") FROM BOOK WHERE YEAR(PUBLISHED_DATE) ="2021" AND CATEGORY = "인문" ORDER BY PUBLISHED_DATE ASC
